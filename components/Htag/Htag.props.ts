262fe62d780de57c314deb348d8529ab6a3f0511
AdelRomeo <11cage11@mail.ru>
2022-03-02 23:01:10 +0700

    components Description and Tag

--- components/Htag/Htag.props.ts
+++ components/Htag/Htag.props.ts
@@ -2,6 +2,6 @@ import {ReactNode} from 'react';
 
 //шаблон принимаемых пропсов
 export interface HtagProps {
-  tag: 'h1' | 'h2' | 'h3',
-  children: ReactNode
+  children: ReactNode,
+  tag: 'h1' | 'h2' | 'h3'
 }
--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,5 @@
 export * from './Htag/Htag';
-export * from './Button/Button'
+export * from './Button/Button';
+export * from './Description/Description';
+export * from './Tag/Tag';
 
