405e0551ba0e385236c9c0a44d82f0912e4e9c14
AdelRomeo <11cage11@mail.ru>
2022-03-11 01:19:22 +0700

    grid

--- hoc/Layout/Footer/Footer.tsx
+++ hoc/Layout/Footer/Footer.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import {FooterProps} from './Footer.props';
-import styles from './Header.module.css';
+import styles from './Footer.module.css';
+import cn from 'classnames'
 
 //компонент заголовка
-export const Footer = ({...props}: FooterProps): JSX.Element => {
+export const Footer = ({className, ...props}: FooterProps): JSX.Element => {
   return (
-   <div  {...props}>
-     Footer
+   <div className={cn(className, styles.footer)} {...props}>
+     <span>OwlTop © 2020 - 2021 Все права защищены</span>
+     <span>Пользовательское соглашение</span>
+     <span>Политика конфиденциальности</span>
    </div>
   );
 };
